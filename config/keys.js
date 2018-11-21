ecd2277cc6fee8cc64474a2718dc6097a4a05945
Tina <tanjilla.tina@gmail.com>
2018-11-21 13:49:53 +0600

    co2

--- config/keys.js
+++ config/keys.js
@@ -2,11 +2,18 @@
 
 module.exports={
 
-
     mongodb:{
-        dbURI:'mongodb://tinadb2:tinadb2@ds041157.mlab.com:41157/courriercost'
+        dbURI:'mongodb://tina:tinadb1@ds037997.mlab.com:37997/passport-outlook-auth'
+        //mongodb://<dbuser>:<dbpassword>@ds037997.mlab.com:37997/passport-outlook-auth
+    },
 
-
-
+    session:{
+        cookieKey:'TanjillaSarkarTina'
+    },
+    outlook:{
+        clientID: '17c855d1-bc76-4111-b885-6246baef6858',
+        clientSecret: 'jahwshoPJD18^)(FKZU361]'
     }
+
+
 };
